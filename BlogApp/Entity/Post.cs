c61331b93910fe8051a672b23ddf437b0b9c817e
Furkanberkay <furkanberkayozcan@hotmail.com>
2025-08-06 11:33:22 +0300

    first commit

---
+++ BlogApp/Entity/Post.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace BlogApp.Entity;
+
+public class Post
+{
+    public int PostId { get; set; }
+    public string? Title { get; set; }
+    public string? Content { get; set; }
+    public DateTime PublishOn { get; set; }
+    public bool IsActive { get; set; }
+    public int UserId { get; set; }
+
+    public User? User { get; set; }
+    public List<Tag> Tags { get; set; } = new List<Tag>();
+    public List<Comment> Comments { get; set; } = new List<Comment>();
+}
---
+++ BlogApp/Entity/User.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace BlogApp.Entity;
+
+public class User
+{
+    public int UserId { get; set; }
+    public string? Username { get; set; }
+    public string? Email { get; set; }
+    public DateTime CreatedOn { get; set; }
+
+    public List<Post> Posts { get; set; } = new List<Post>();
+    public List<Comment> Comments { get; set; } = new List<Comment>();
+
+}
